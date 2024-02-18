3d8f01fcbd1a33ab4c65568ea1f0ad3a3217d557
MichaelCache <faith_gentoo@163.com>
2024-02-18 17:41:17 +0800

    <feat>reconstruct refereced cell tree

--- src/gdsdk/gds_parser.rs
+++ src/gdsdk/gds_parser.rs
@@ -1,6 +1,10 @@
 use super::gds_error::*;
+use super::gds_model;
 use super::gds_model::*;
 use super::gds_record::*;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
 use std::slice::Iter;
 
 pub fn parse_gds(records: &[Record]) -> Result<Box<Lib>, Box<dyn Error>> {
@@ -21,6 +25,8 @@ pub fn parse_gds(records: &[Record]) -> Result<Box<Lib>, Box<dyn Error>> {
 fn parse_lib(iter: &mut Iter<'_, Record>) -> Result<Box<Lib>, Box<dyn Error>> {
     let mut lib = Box::new(Lib::default());
     let mut factor = 0.0;
+    let mut name_cell_map = HashMap::new();
+    // step.1 parse all cell, save to name_cell_map
     while let Some(record) = iter.next() {
         match record {
             Record::BgnLib(date) => lib.date = date.clone(), //modification time of lib, and marks beginning of library
@@ -42,7 +48,14 @@ fn parse_lib(iter: &mut Iter<'_, Record>) -> Result<Box<Lib>, Box<dyn Error>> {
             }
             Record::BgnStr(_) => {
                 let cell = parse_cell(iter, factor)?;
-                lib.cells.push(cell);
+                if name_cell_map.contains_key(&(*cell).borrow_mut().name) {
+                    return Err(Box::new(gds_err(&std::format!(
+                        "Duplicated Cell \"{}\" found",
+                        &(*cell).borrow_mut().name
+                    ))));
+                }
+                let cell_name = (*cell).borrow_mut().name.clone();
+                name_cell_map.insert(cell_name, cell);
             }
             Record::EndLib => {
                 break;
@@ -53,34 +66,54 @@ fn parse_lib(iter: &mut Iter<'_, Record>) -> Result<Box<Lib>, Box<dyn Error>> {
         }
     }
 
+    // step.2 connect reference to cell, only cell not be refered
+    let mut not_refered_cell = name_cell_map.clone();
+    for c in &name_cell_map {
+        let mut cell = c.1.borrow_mut();
+        for refer in cell.refs.iter_mut() {
+            if let gds_model::RefCell::CellName(name) = &mut refer.refed_cell {
+                let refered_cell = name_cell_map.get(name).unwrap();
+                not_refered_cell.remove(name);
+                refer.refed_cell = gds_model::RefCell::Cell(refered_cell.clone());
+            }
+        }
+    }
+
+    for c in not_refered_cell {
+        lib.cells.push(c.1);
+    }
+
     Ok(lib)
 }
 
-fn parse_cell(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Box<Cell>, Box<dyn Error>> {
-    let mut cell = Box::new(Cell::default());
+fn parse_cell(
+    iter: &mut Iter<'_, Record>,
+    factor: f64,
+) -> Result<Rc<RefCell<Cell>>, Box<dyn Error>> {
+    let cell = Rc::new(RefCell::new(Cell::default()));
     while let Some(record) = iter.next() {
         match record {
-            Record::BgnStr(date) => cell.date = date.clone(), // last modification time of a structure and marks the beginning of a structure
-            Record::StrName(s) => cell.name = s.to_string(),
+            Record::BgnStr(date) => (*cell).borrow_mut().date = date.clone(), // last modification time of a structure and marks the beginning of a structure
+            Record::StrName(s) => (*cell).borrow_mut().name = s.to_string(),
             Record::Boundary => {
                 let polygon = parse_polygon(iter, factor)?;
-                cell.polygons.push(polygon);
+                (*cell).borrow_mut().polygons.push(polygon);
             }
             Record::Path => {
                 let path = parse_path(iter, factor)?;
-                cell.paths.push(path);
+                (*cell).borrow_mut().paths.push(path);
             }
             Record::StrRef => {
                 let sref = parse_sref(iter, factor)?;
-                cell.refs.push(sref);
+                (*cell).borrow_mut().refs.push(sref);
             }
             Record::Text => {
                 let text = parse_text(iter, factor)?;
-                cell.label.push(text)
+                (*cell).borrow_mut().label.push(text)
             }
             Record::AryRef => {
                 let aref = parse_aref(iter, factor)?;
-                cell.refs.push(aref);
+                (*cell).borrow_mut().refs.push(aref);
             }
             Record::EndStr => {
                 break;
@@ -135,8 +168,9 @@ fn parse_text(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Text, Box<dyn 
             Record::Angle(angle) => text.rotation = std::f64::consts::PI / 180.0 * angle,
             Record::RefTrans {
                 reflection_x,
-                absolute_magnification,
-                absolute_angle,
+                ..
+                // absolute_magnification,
+                // absolute_angle,
             } => text.x_reflection = *reflection_x,
             Record::Points(points) => {
                 text.position =
@@ -192,19 +226,20 @@ fn parse_path(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Path, Box<dyn 
 }
 
 fn parse_sref(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Ref, Box<dyn Error>> {
-    let mut sref = Ref::default();
+    let mut sref = Ref::new();
     while let Some(record) = iter.next() {
         match record {
             Record::StrRef => (), // marks the beginning of an SREF(structure reference) element
-            Record::StrRefName(s) => sref.refed_cell = RefCell::CellName(s.to_string()),
+            Record::StrRefName(s) => sref.refed_cell = gds_model::RefCell::CellName(s.to_string()),
             Record::RefTrans {
                 reflection_x,
-                absolute_magnification,
-                absolute_angle,
+                ..
+                // absolute_magnification,
+                // absolute_angle,
             } => {
                 sref.reflection_x = *reflection_x;
-                sref.abs_magnific = *absolute_magnification;
-                sref.abs_angel = *absolute_angle;
+                // sref.abs_magnific = *absolute_magnification;
+                // sref.abs_angel = *absolute_angle;
             }
             Record::MAG(mag) => sref.magnific = *mag,
             Record::Angle(angle) => sref.angle = std::f64::consts::PI / 180.0 * angle,
@@ -221,19 +256,20 @@ fn parse_sref(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Ref, Box<dyn E
 }
 
 fn parse_aref(iter: &mut Iter<'_, Record>, factor: f64) -> Result<Ref, Box<dyn Error>> {
-    let mut aref = Ref::default();
+    let mut aref = Ref::new();
     while let Some(record) = iter.next() {
         match record {
             Record::AryRef => (), // marks the beginning of an SREF(structure reference) element
-            Record::StrRefName(s) => aref.refed_cell = RefCell::CellName(s.to_string()),
+            Record::StrRefName(s) => aref.refed_cell = gds_model::RefCell::CellName(s.to_string()),
             Record::RefTrans {
                 reflection_x,
-                absolute_magnification,
-                absolute_angle,
+                ..
+                // absolute_magnification,
+                // absolute_angle,
             } => {
                 aref.reflection_x = *reflection_x;
-                aref.abs_magnific = *absolute_magnification;
-                aref.abs_angel = *absolute_angle;
+                // aref.abs_magnific = *absolute_magnification;
+                // aref.abs_angel = *absolute_angle;
             }
             Record::MAG(mag) => aref.magnific = *mag,
             Record::Angle(angle) => aref.angle = std::f64::consts::PI / 180.0 * angle,
